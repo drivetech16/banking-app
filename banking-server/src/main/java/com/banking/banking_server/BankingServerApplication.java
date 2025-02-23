9ddce1a2f14b7f43eeb2553e3c7407735dcdf480
ranjith <ranjithraki07@gmail.com>
2025-02-23 12:21:06 +0530

    Back End Added Succesfully

---
+++ banking-server/src/main/java/com/banking/banking_server/BankingServerApplication.java
@@ -0,0 +1,13 @@
+package com.banking.banking_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingServerApplication.class, args);
+	}
+
+}
